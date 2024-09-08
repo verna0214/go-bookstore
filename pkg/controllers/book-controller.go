266ace0fec3983108776f017494acc2a9d008f2d
verna0214 <yuhsichen0214@gmail.com>
2024-09-08 13:43:56 +0800

    feat: add GetBookById func controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -19,3 +19,17 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Connect-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
